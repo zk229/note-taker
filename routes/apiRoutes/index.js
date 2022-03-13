937ff211116be0245f6c320684cbd5a1a36294e5
zk229 <zkirsche01@gmail.com>
2022-03-13 18:19:55 -0400

    set up API routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,4 +1,23 @@
-const { notes } = require("../../db/db.json");
+const notes = require("../../db/db.json");
 const router = require("express").Router();
+const uuid = require("uuid");
+const { validateNote, writeNote } = require("../../lib/notes");
+
+router.get("/notes", (req, res) => {
+    res.json(notes);
+});
+
+router.post("/notes", (req, res) => {
+    req.body.id = uuid.v4();
+
+    if(!validateNote(req.body)) {
+        res.status(400).send("The note is not properly formatted");
+    }
+    else{
+        const note = writeNote(req.body);
+        res.json(note);
+    }
+    
+});
 
 module.exports = router;
